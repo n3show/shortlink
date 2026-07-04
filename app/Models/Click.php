844ff35acafc82cc53b83fdda1156bafb60fbd1f
Muxsin <muxsin.shokirov@gmail.com>
2026-07-04 19:22:34 +0700

    implemented models

--- app/Models/Click.php
+++ app/Models/Click.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Click extends Model
 {
-    //
+    public $timestamps = false;
+
+    protected $fillable = [
+        'link_id',
+        'ip_address',
+        'clicked_at',
+    ];
+
+    public function link(): BelongsTo
+    {
+        return $this->belongsTo(link::class);
+    }
 }
--- app/Models/Link.php
+++ app/Models/Link.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Link extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'original_url',
+        'short_code',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function clicks(): HasMany
+    {
+        return $this->hasMany(Click::class);
+    }
 }
